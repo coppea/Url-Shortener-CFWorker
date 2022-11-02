2971feab11e7d267ce12e8eb33832e4813d7a356
coppea <86509331+coppea@users.noreply.github.com>
2022-11-02 14:19:47 +0800

    第一次上传

---
+++ README.md
@@ -0,0 +1,77 @@
+# CFWork-Url-Shorten
+使用Cloudflare Worker创建的URL缩短器｜支持自定义首页｜支持Menu Short｜支持短网址和文本分享
+
+**首页**
+
+<img width="800" alt="首页" src="https://user-images.githubusercontent.com/86509331/199412206-b4aa8b77-ab4c-4328-8143-cc7e0974a235.png">
+
+**后台**
+
+<img width="800" alt="后台" src="https://user-images.githubusercontent.com/86509331/199411957-aa39205b-d9c1-4b3a-b9bb-26d1aed824b2.png">
+
+
+## 常量设置
+```js
+const admin_path = '/admin_err24d' // 访问地址，如果需要公开访问,把`/admin_err24d`设置成`/`即可
+const api_path = '/vcfgbn_api'  //Short Menu第三方访问api，建议设置复杂
+const url_key = 'longUrl'  //原始长链接
+const url_name = 'shortCode' // 自定义后缀  
+const short_url_key = 'shorturl';   //完整缩短后链接，'shorturl'是为了兼容Short Menu
+```
+网址响应
+```js
+{
+ "longUrl":"https://abc.com",
+ "shortCode":"dfcw",
+ "type":"link",
+ "shorturl":"https://url.com/dfcw"
+}
+```
+文本响应
+```js
+{
+ "longUrl":"这是测试文本",
+ "shortCode":"dfce",
+ "type":"text",
+ "shorturl":"https://url.com/dfce"
+}
+```
+## 自定义首页[index.js](../04d9c11c960017f4e0b1aea5b518019602526f8f/index.js#L163)
+
+```js
+const statichtml = "https://raw.githubusercontent.com/bituplink/OneHtmlNav/master/goodweb.html"
+```
+修改上方网址为自己喜欢的网址，只支持单html，建议css和js和script集成进html里
+
+-------
+## [cloudflare设置](https://dash.cloudflare.com)
+
+### 去Workers KV中创建一个命名空间
+
+<img width="1020" alt="去Workers KV中创建一个命名空间" src="https://user-images.githubusercontent.com/86509331/199402623-677c28da-f5de-42d0-b7f9-b652c89f0311.png">
+
+
+### 去Worker的设置选选项卡中绑定KV 命名空间
+<img width="1020" alt="绑定KV Namespace 1" src="https://user-images.githubusercontent.com/86509331/199405088-38963240-e967-4a69-921e-f06299d403c1.png">
+
+
+<img width="1020" alt="绑定KV Namespace 2" src="https://user-images.githubusercontent.com/86509331/199405413-c117ed63-2cf9-4fad-a89e-327cc7b137c2.png">
+
+
+### 其中变量名称填写`shortlink`, KV 命名空间填写你刚刚创建的命名空间
+
+<img width="1020" alt="绑定kv" src="https://user-images.githubusercontent.com/86509331/199404159-9e3c14e9-22ca-4957-958f-19f9ab0af105.png">
+
+
+### 复制本项目中的[index.js](/index.js)的代码到Cloudflare Worker 
+
+### 点击保存并部署
+
+-------
+
+### Short Menu设置截图
+
+![Upic设置](https://user-images.githubusercontent.com/86509331/199400317-929ee122-b5d0-4cb6-9e9b-0362cb6afbd4.PNG)
+---------
+## 短链接来自于[@xyTom](https://github.com/xyTom/Url-Shorten-Worker)和[@code-scan](https://github.com/code-scan/cfwork_shortlink_text)
+## 自定义首页来自[@bituplink](https://github.com/bituplink/OneHtmlNav)
---
+++ index.js
@@ -0,0 +1,220 @@
+const admin_path = '/admin'
+const api_path = '/fkrrf_api'
+const url_key = 'longUrl' // original url key
+const url_name = 'shortCode' // short code  key
+const short_url_key = 'shorturl'; // full short url
+
+const index = `<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link href="https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔗</text></svg>">
+
+    <title>短网址生成</title>
+</head>
+
+<style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+    }
+    
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
+    
+    .btn-secondary,
+    .btn-secondary:hover,
+    .btn-secondary:focus {
+        color: #333;
+        text-shadow: none;
+    }
+    
+    body {
+        text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+    }
+    
+    .cover-container {
+        max-width: 42em;
+    }
+    
+    .nav-masthead .nav-link {
+        padding: .25rem 0;
+        font-weight: 700;
+        color: rgba(255, 255, 255, .5);
+        background-color: transparent;
+        border-bottom: .25rem solid transparent;
+    }
+    
+    .nav-masthead .nav-link:hover,
+    .nav-masthead .nav-link:focus {
+        border-bottom-color: rgba(255, 255, 255, .25);
+    }
+    
+    .nav-masthead .nav-link+.nav-link {
+        margin-left: 1rem;
+    }
+    
+    .nav-masthead .active {
+        color: #fff;
+        border-bottom-color: #fff;
+    }
+</style>
+
+</head>
+
+<body class="d-flex h-100 text-center text-white bg-dark">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <!--div>
+                <h3 class="float-md-start mb-0">短链接</h3>
+
+            </div-->
+        </header>
+        <br>
+        <br>
+        <main class="px-3">
+            <p id="result" class="lead"></p>
+
+            <br>
+            <div id="link_div" class="input-group mb-3">
+                <!-- <input type="text" id="link" class="form-control" placeholder="link" aria-label="link"> -->
+                <select class="form-control" id="select">
+                    <option value="link">🔗 链接</option>
+                    <option value="text">📄 文本</option>
+                </select>
+                <input type="text" id="name" placeholder="自定义后缀" class="input-group-text">
+            </div>
+            <div id="text_div">
+                <textarea id="link" placeholder="输入网址/文本" class="form-control" rows="10"></textarea><br>
+            </div>
+            <p class="lead">
+                <a href="#" onclick="getlink()" class="btn btn-lg btn-secondary fw-bold border-white bg-white">生成</a>
+            </p>
+        </main>
+
+
+
+
+    </div>
+    <script>
+        async function postData(url = '', data = {}) {
+            // Default options are marked with *
+            const response = await fetch(url, {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    'Content-Type': 'application/json'
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: JSON.stringify(data) // body data type must match "Content-Type" header
+            });
+            return response.json(); // parses JSON response into native JavaScript objects
+        }
+
+        function getlink() {
+            document.getElementById('result').innerHTML = "processing.."
+            let link = document.getElementById('link').value
+            const name = document.getElementById('name').value
+            const type = document.getElementById('select').value
+            if (link.indexOf('http') == -1 && type == "link") {
+                link = 'http://' + link
+            }
+            postData("${api_path}", {
+                "${url_key}": link,
+                "${url_name}": name,
+                "type": type
+            }).then(resp => {
+                var url = document.location.protocol + '//' + document.location.host + '/' + resp['${url_name}']
+                document.getElementById('result').innerHTML = url
+                document.getElementById('name').value = resp['${url_name}']
+            })
+        }
+    </script>
+
+</html>`
+
+
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+ //自定义首页
+const statichtml = "https://raw.githubusercontent.com/bituplink/OneHtmlNav/master/goodweb.html"
+
+async function handleRequest(request) {
+    const { protocol, hostname, pathname } = new URL(request.url);
+    // index.html
+    if (pathname == admin_path) {
+        return new Response(index, {
+            headers: { 'content-type': 'text/html; charset=utf-8' },
+        })
+    }
+    // short api
+    if (pathname.startsWith(api_path)) {
+        const body = JSON.parse(await request.text());
+        console.log(body)
+        var short_type = 'link'
+        if (body['type'] != undefined && body['type'] != "") {
+            short_type = body['type'];
+        }
+
+        if (body[url_name] == undefined || body[url_name] == "" || body[url_name].length < 2) {
+            body[url_name] = Math.random().toString(36).slice(-5)
+        }
+        await shortlink.put(body[url_name], JSON.stringify({
+            "type": short_type,
+            "value": body[url_key]
+        }))
+        body[short_url_key] = `${protocol}//${hostname}/${body[url_name]}`
+        return new Response(JSON.stringify(body), {
+            headers: { "Content-Type": "text/plain; charset=utf-8" },
+        });
+    }
+    const key = pathname.replace('/', '')
+    if (key == "") {
+        const html = await fetch(statichtml)
+        return new Response(await html.text(), {
+            headers: {
+                "content-type": "text/html;charset=UTF-8",
+            },
+        })
+    }
+    let link = await shortlink.get(key)
+    if (link != null) {
+        link = JSON.parse(link)
+        console.log(link)
+            // redirect
+        if (link['type'] == "link") {
+            return Response.redirect(link['value'], 302);
+        } else {
+            // textarea
+            return new Response(`${link['value']}`, {
+                headers: { 'content-type': 'text/plain; charset=utf-8' },
+            })
+        }
+    }
+    return new Response(`403`, {
+        headers: { 'content-type': 'text/plain; charset=utf-8' },
+    })
+}
